c9c1d2ed147dc33523ca9c13d8a56c011c93b163
hassen <hassen.ahmadi@esprit.tn>
2022-12-14 02:40:16 +0100

    some fixes

--- Routes/Message.js
+++ Routes/Message.js
@@ -1,15 +1,13 @@
 import express from"express";
 const router = express.Router()
 import {getAllConversations,getAllMessages,getMyMessages,getMyConversations,createConversation,sendMessage,deleteConversation,deleteMessage,deleteAll} from "../Controller/MessageController.js";
-
-router.route("/dis").get(getAllConversations);
-router.route("/dis/tout-messages").get(getAllMessages);
-router.route("/dis/my-conversations/:senderId").get(getMyConversations);
-router.route("/dis/my-messages/:conversationId").get(getMyMessages);
-router.route("/dis/create-conversation").post(createConversation);
-router.route("/dis/sendmessage").post(sendMessage);
-router.route("/dis").delete(deleteConversation);
-router.route("/dis/message").delete(deleteMessage);
-router.route("/dis/deleteAll").delete(deleteAll);
-
+router.route("/").get(getAllConversations);
+router.route("/tout-messages").get(getAllMessages);
+router.route("/my-conversations/:senderId").get(getMyConversations);
+router.route("/my-messages/:conversationId").get(getMyMessages);
+router.route("/create-conversation").post(createConversation);
+router.route("/send-message").post(sendMessage);
+router.route("/").delete(deleteConversation);
+router.route("/message").delete(deleteMessage);
+router.route("/deleteAll").delete(deleteAll);
 export default router;
